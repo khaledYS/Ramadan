e1ffefa5e73ea6221806309f21deffa1f47fc274
Your Name <you@example.com>
2022-03-14 17:32:45 +0300

    updated and a looooooot of code refactoring

---
+++ src/pages/ramadan/components/headSection/HeadSection.jsx
@@ -0,0 +1,29 @@
+import MotionFadeInUp from "../../../../components/motionAnimatedComponents/fadeInUp/fadeInUp";
+import { filterObject } from "../../../../utils";
+
+function HeadSection(props) {
+    return (
+        <div
+         {...filterObject(props, ["className", "lang", "setLang"])}
+         className="reveal w-full h-full text-[15vw] grid place-items-center snap"
+        >
+            <MotionFadeInUp once>
+                <a 
+                 href="#sec1" 
+                 className=" block cursor-pointer relative text-[#5BA499]"
+                >
+                    { props.lang == "ar" && <h1 
+                                        className="ar welcome-ramadan-text" 
+                                        data-text="رمضان"
+                                        > رمضان</h1>}
+                    { props.lang == "en" && <h1 
+                                        className="en welcome-ramadan-text"
+                                        data-text="Ramadan"
+                                        >Ramadan</h1>}
+                </a>
+            </MotionFadeInUp>
+        </div>
+    );
+}
+
+export default HeadSection;
